b40129edd5d9f0b8db2282eb98064f731a263d46
Ahmed Bedair <ahmed.bedair@kcl.ac.uk>
2022-10-03 08:14:47 +0100

    Completed Task

--- primes.py
+++ primes.py
@@ -3,4 +3,17 @@
 
 def primes(number_of_primes):
     list = []
+    count = 2
+    if number_of_primes <= 0:
+        raise ValueError
+        return list
+    list_length = number_of_primes
+    while list_length != 0:
+        for x in range(2,count):
+            if count % x == 0:
+                break
+        else:
+            list.append(count)
+            list_length -= 1
+        count += 1
     return list
